d4eb2a4de271a9e142b83396f239d5eb6956c3ce
mhosnyessa <mhosny67710@gmail.com>
2022-12-09 21:55:59 +0200

    resolving some errors related to git ignore

--- c_codebase/simple_tic_tac_toe/main.c
+++ c_codebase/simple_tic_tac_toe/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <conio.h>
 
-
-int main(){
-	int player = 1; //first player --> 1 second player --> 2
-	int choice = NULL; //indicates the current choice
+int main()
+{
+	int player = 1;	   // first player --> 1 second player --> 2
+	int choice = NULL; // indicates the current choice
 }
