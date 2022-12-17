b9e3ee3a7318dff8ba96d0e3dd7389cc98b94273
ranatarek-rt <64408710+ranatarek-rt@users.noreply.github.com>
2022-12-17 19:39:39 +0200

    Update biddingx_o.asm

--- Assembly_BiddingBoard/biddingx_o.asm
+++ Assembly_BiddingBoard/biddingx_o.asm
@@ -5,8 +5,13 @@ org  100h	; set location counter to 100h
 .data
   msg1 DB 'player no 1 enter a number to bid on', '$'
   msg2 DB  'player no 2 enter a number to bid on', '$'
- bid_player1 db 1 dup<?> 
- bid_player2 db 1 dup<?>
+  msg3 DB  'bidding numbers are equal please re enter the numbers','$'
+ string db 5 ;MAX NUMBER OF CHARACTERS ALLOWED (4).
+       db ? ;NUMBER OF CHARACTERS ENTERED BY USER.
+       db 5 dup (?) ;CHARACTERS ENTERED BY USER. 
+
+player_bidding DB ?
+
 
 
 .code 
@@ -38,19 +43,49 @@ org  100h	; set location counter to 100h
     
     ret   
     newline endp  
+     
+    ;************************func to convert string to integer value ***************************
+     
+    ;CONVERT STRING TO NUMBER IN BX.
+    proc string2number         
+        ;MAKE SI TO POINT TO THE LEAST SIGNIFICANT DIGIT.
+          mov  si, offset string + 1 ;
+          mov  cl, [ si ]                                        
+          mov  ch, 0 
+          add  si, cx 
+        ;CONVERT STRING.
+          mov  bx, 0
+          mov  bp, 1 ;MULTIPLE OF 10 TO MULTIPLY EVERY DIGIT.
+        repeat:         
+        ;CONVERT CHARACTER.                    
+          mov  al, [ si ] ;CHARACTER TO PROCESS.
+          sub  al, 48 
+          mov  ah, 0 ;
+          mul  bp ;AX*BP = DX:AX.
+          add  bx,ax          
+          mov  ax, bp
+          mov  bp, 10
+          mul  bp 
+          mov  bp, ax  
+        ;CHECK IF WE HAVE FINISHED.
+          dec  si 
+          loop repeat 
+        
+      ret 
+    string2number endp  
     
-    
-    
+   
     ;bidding function
     
-    bidding_board proc 
-        
+    bidding_board proc  
+         label1:    
+         call newline  
          MOV AX,@DATA 
          MOV DS,AX 
-          
+         
          ; load address of the string1
          LEA DX,msg1
-          
+         
          ;output the string
          ;loaded in dx 
          MOV AH,09H
@@ -59,36 +94,32 @@ org  100h	; set location counter to 100h
          call newline   
          
          
-        ;take multiple character input from user and save it in a variable 
-        mov ax,@data
-        mov ds,ax
-        mov ax,0000
-        
-        
-        mov si,offset bid_player1
-        
-        l1: 
-        mov ah,01h
-        int 21h
-        mov [si],al
-        inc si
-        cmp al,'$'
-        jne l1
-          
-        call newline  
-       
-        mov ah,09h
-        lea dx,bid_player1
-        int 21h   
-        
-        
-        
+         
+         ;*********************** new code ******************************************* 
+         
+                  ;------------------------------------------
+        ;CAPTURE CHARACTERS (THE NUMBER).
+          mov  ah, 0Ah
+          mov  dx, offset string
+          int  21h
+        ;------------------------------------------
+          call string2number
+          mov di,BX       
+          cmp di,101 ; si contain the first player bid number
+          JGE label1  
+          jmp label2  
+             
+             
+         ;*************** ask the second user for bidding number *****************
+         
+             
+         label2:       
          ; display the second message for the user 
          call newline
            
          MOV AX,@DATA 
          MOV DS,AX 
-          
+                   
          ; load address of the string1
          LEA DX,msg2
           
@@ -99,28 +130,56 @@ org  100h	; set location counter to 100h
          ;call function to print new line between the strings   
          call newline   
             
-        ;take multiple character input from user and save it in a variable 
-        mov ax,@data
-        mov ds,ax
-        mov ax,0000
-        
-        
-        mov si,offset bid_player2
-        
-        l2: 
-        mov ah,01h
-        int 21h
-        mov [si],al
-        inc si
-        cmp al,'$'
-        jne l2
+          ;CAPTURE CHARACTERS (THE NUMBER).
+          mov  ah, 0Ah
+          mov  dx, offset string
+          int  21h
+        ;------------------------------------------
+          call string2number
+          call newline      
+          cmp BX,101 ; Dx contain the second player bid number
+          JGE label2  
+          jmp check_label 
           
-        call newline  
-       
-        mov ah,09h
-        lea dx,bid_player2
-        int 21h   
+         
+       ;***************** check who has the right to play ****************** if else statement***********
+         ;Di =bid_player1 BX=bid_player2 
+         check_label:
+         mov AX,0 ;player1_count
+         mov dx,0 ;player2_count
+          
+         cmp Di,BX 
+         JG check_label2 
+         JL check_label3
+         
+         ; load address of the msg3
+         LEA DX,msg3
+         
+         ;output the string
+         ;loaded in dx 
+         MOV AH,09H
+         INT 21H 
+        
+         Jmp label1
+         
+          
+        check_label2: ;si>di
+        sub AX,di
+        add AX,di 
+        mov player_bidding,1
+        ;here we need to call board function to play   
+        
+         
+        check_label3:
+        sub dx,BX
+        add AX,BX  
+        mov player_bidding,2  
+        ;here we need to call board function to play   
+        
 
+       
+
+ 
       ret 
       
      bidding_board endp
@@ -129,8 +188,11 @@ org  100h	; set location counter to 100h
     
      
   end main
-
-ret  
+            
+ 
+            
+         
+ret   ; return to the operating system.
 
 
 
