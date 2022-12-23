b3282488daba456ab9c7034282b04f629a1a2854
Aya-abdo27 <66348016+Aya-abdo@users.noreply.github.com>
2022-12-23 17:38:22 +0200

    Update Main_code.asm

--- Main_code.asm
+++ Main_code.asm
@@ -13,10 +13,45 @@ Draw db 'Game Draw',0dh,0ah,'$'
 Player1 db 'Player 1 won!',0dh,0ah,'$'
 Player2 db 'Player 2 won!',0dh,0ah,'$' 
 Computer db 'Computer won!',0dh,0ah,'$'
-PlayerWon db 'Player won!',0dh,0ah,'$'
+PlayerWon db 'Player won!',0dh,0ah,'$'   
 
-  
-SQUARE	db  0,1,2,3,4,5,6,7,8,9 
+
+msg1 DB 'player no 1 enter a number to bid on', '$'
+msg2 DB  'player no 2 enter a number to bid on', '$'
+msg3 DB  'bidding numbers are equal please re enter the numbers','$'
+string db 5 ;MAX NUMBER OF CHARACTERS ALLOWED (4).
+        db ? ;NUMBER OF CHARACTERS ENTERED BY USER.
+        db 5 dup (?) ;CHARACTERS ENTERED BY USER. 
+
+
+
+
+enter_choice db 'Enter your Choice = ',0dh,0ah,'$' 
+enter_valid db 'Enter Valid number',0dh,0ah,'$'
+
+
+prompt_1 db 'player','$'
+prompt_2 db ' : please enter a position', 0AH, 0DH, '$' 
+err_msg db 'the position is already taken', 0AH,0DH,'$'
+invalid_msg db 'Invalid position', 0AH, 0DH, '$'
+
+counter dw 0
+;----------------\\
+letter db  'X'
+position db 0
+flag db 0   
+JUMP db 0
+
+empty_line db '     |     |     ',0Ah,0DH,'$'   ;0DH moves to the curser to the beginning
+                                                                                                                  
+empty_line_dashed db '_____|_____|_____',0Ah,0DH,'$'
+
+option_1 db 'Player 1 (X) - Player 2 (O)',0AH, 0DH, 0AH, 0DH, 0AH, 0DH, '$'
+option_2 db 'Player 1 (X) - Player 2 (O)',0AH, 0DH ,'         (Bidding) ',0AH, 0DH,0AH, 0DH,0AH, 0DH, '$'
+option_3 db 'Player (X) - Computer (O)', 0AH, 0DH,0AH, 0DH, '$'
+
+
+SQUARE	db  '1','2','3','4','5','6','7','8','9'
 
 
 player db 0
@@ -115,22 +150,29 @@ Re:
     mov BYTE PTR [si], al
     mov bl, option
     cmp bl, 2
-    je Else 
+    je Else2 
     cmp bl, 3
     je Else_If 
      
-do:     
-    ;jmp Board()
-    ;jmp check_valid() 
-    ;jmp Board()
+do: 
+    mov ax, 13h     ; Clear Screen
+    int 10h     
+    call board
+    call check_valid
+    mov ax, 13h     ; Clear Screen
+    int 10h  
+    call board
+    mov ax, 13h     ; Clear Screen
+    int 10h 
     
     mov al, player  
     add al, 1
     mov si,offset player
     mov BYTE PTR [si], al 
     
-    ;jmp checkwin() then take ret value at ax
-    
+    call check 
+     
+     
     cmp ax, -1
     je do           ; while loop
     
@@ -139,7 +181,7 @@ do:
     mov si,offset player
     mov BYTE PTR [si], al   
     
-    cmp cx, 0
+    cmp ax, 0
     je Equal
     
     mov dl, al
@@ -162,18 +204,23 @@ R9:
        
     
     
-Else: 
-     ;jmp isEmpty()        ; get value if 1 or 0
+Else2: 
+     call isEmpty        ; get value if 1 or 0
      cmp ax, 0
      je bot
      
-     ;jmp Board()
-     ;jmp Bidding_Board()
+     mov ax, 13h     ; Clear Screen
+     int 10h 
+     call board
+     call bidding_board
      
-     ;jmp checkwin() then take ret value at ax
+     call check
 
-     cmp ax, 1
-     je Print
+     cmp ax, -1
+     je Else2
+     
+     cmp ax, 0
+     jne Print
      mov ah,09h      ; print phrase
      mov dx, offset Draw 
      int 21h
@@ -203,18 +250,27 @@ Else_If:
  
     
 do2:    
-    ;jmp Board()
-    ;jmp check_valid() 
-    ;jmp Board()
+    mov ax, 13h     ; Clear Screen
+    int 10h   
+    call board
+P1: 
+    call check_valid  
+    mov ax, 13h     ; Clear Screen
+    int 10h  
+    call board
+p2:    
+    mov ax, 13h     ; Clear Screen
+    int 10h 
     
     mov al, player  
     add al, 1
     mov si,offset player
     mov BYTE PTR [si], al 
     
-    ;jmp checkwin() then take ret value at cx
+    call check
+  
+    cmp ax, -1
     
-    cmp cx, -1
     je do2           ; while loop
     
     mov al, player  
@@ -222,7 +278,7 @@ do2:
     mov si,offset player
     mov BYTE PTR [si], al   
     
-    cmp cx, 0
+    cmp ax, 0
     je Equal
     
     mov dl, al
@@ -271,14 +327,4 @@ NNEqual2:
     mov dx, offset PlayerWon 
     int 21h   
        
-    jmp bot    
-        
-
-
-    
-bot:
-    
- 
-
-.exit:
-END
+    jmp bot
